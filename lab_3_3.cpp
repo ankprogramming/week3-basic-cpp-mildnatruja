8295ceb3f5f4e77e2756ecb2fff9d1f51aa0408c
Unknown <mildnatruja@gmail.com>
2017-11-28 11:42:43 +0700

    ข้อ3 เสร็จ

--- lab_3_3.cpp
+++ lab_3_3.cpp
@@ -3,6 +3,22 @@
 using namespace std;
 
 int main() {
-  
+
+   const float pi = 3.14345926536;
+   float r;
+   int k = 2 , h = 15;
+   float area;
+
+   cout << "Please enter radius :";
+   cin >> r;
+   area = pi * r * r;
+   cout << "Area of Circle :" <<area <<endl;
+
+   cout <<"Circumference of Circle : "<<k*pi*r<<endl;
+
+   cout <<"Cylindrical space ; "<<pi*(r*r)*h<<endl;
+
+
+
   return 0;
 }
